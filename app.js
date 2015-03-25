0012284bda6ce8fa9a796740322b7b9094a689ac
cyuseff <cr.yuseff@gmail.com>
2015-03-25 14:18:13 -0300

    redis togo added

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 	client = require("redis").createClient(rtg.port, rtg.hostname);
 	client.auth(rtg.auth.split(":")[1]);
-	client.select('production'.length);
 } else {
 	client = redis.createClient();
 	client.select('development'.length);

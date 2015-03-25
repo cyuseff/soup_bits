37281455a2e1fe9821852c47d3d4f96c9ea29397
cyuseff <cr.yuseff@gmail.com>
2015-03-25 11:35:49 -0300

    Add cities

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({extended:false});
+
+
 app.use(express.static('public'));
 
-app.get('/cities', function(request, response){
+var cities = {
+		'Lotopia':'description for Lotopia',
+		'Caspiana':'description for Caspiana',
+		'Indigo':'description for Indigo'
+	};
 
-	var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-	response.json(cities);
-	
+app.get('/cities', function(request, response){
+	response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencode, function(request, response){
+	var newCity = request.body;
+	cities[newCity.name] = newCity.description;
+	response.status(201).json(newCity.name);
 });
 
 module.exports = app;
